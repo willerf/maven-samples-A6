2c85e941db4bbe7d3db9940b71619be89e24c5d8
dhetong <dhetong0714@gmail.com>
2024-03-13 15:30:51 -0400

    bug 2

--- single-module/src/test/java/com/example/TestA6.java
+++ single-module/src/test/java/com/example/TestA6.java
@@ -19,7 +19,7 @@ public class TestA6 {
         String test_str_0 = "abcd";
         String test_str_1 = "abcdefg";
 
-        assertFalse("str shorter than 5", a6_tester.length_check(test_str_0));
+        assertTrue("str shorter than 5", a6_tester.length_check(test_str_0));
         assertFalse("str longer than 5", a6_tester.length_check(test_str_1));
     }
 }
